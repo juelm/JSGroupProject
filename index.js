9674d0ca7d9cef0ddc166ae94ea4578907452a55
xobre <mua.breana@gmail.com>
2019-12-02 09:03:57 -0800

    Comment out

--- index.js
+++ index.js
@@ -10,29 +10,29 @@ let ctx = can.getContext("2d");
 document.addEventListener("keydown", keydown);
 document.addEventListener("keyup", keyup);
 
-function keydown(/** @type {keyboarkdEvent}*/ev){
+// function keydown(/** @type {keyboarkdEvent}*/ev){
     
-    switch(ev.keyCode){
-        case 37: //Left Arrow
-            break;
-        case 38: //Up Arrow
-            break;
-        case 39: //Right Arrow
-            break;
+//     switch(ev.keyCode){
+//         case 37: //Left Arrow
+//             break;
+//         case 38: //Up Arrow
+//             break;
+//         case 39: //Right Arrow
+//             break;
 
-    }
-}
-function keyup(/** @type {keyboarkdEvent}*/ ev){
-    switch(ev.keyCode){
-        case 37: //Left Arrow
-            break;
-        case 38: //Up Arrow
-            break;
-        case 39: //Right Arrow
-            break;
+//     }
+// }
+// function keyup(/** @type {keyboarkdEvent}*/ ev){
+//     switch(ev.keyCode){
+//         case 37: //Left Arrow
+//             break;
+//         case 38: //Up Arrow
+//             break;
+//         case 39: //Right Arrow
+//             break;
 
-    }
-}
+//     }
+// }
 
 setInterval(update, 1000 / FPS);
 
