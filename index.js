acb85f8b2e5ed3cb80d3dccdabd38f9c5a61404f
xobre <mua.breana@gmail.com>
2019-12-03 08:27:43 -0800

    update master

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const FPS = 30;
+const FPS = 60;//30;
 
 let can = document.getElementById('gameCanvas');
 let ctx = can.getContext('2d');
@@ -56,13 +56,17 @@ class polygonalTrack {
 
 let xy = 40;
 let p = 0;
-setInterval(update, 1000 / FPS);
+//setInterval(update, 1000 / FPS);
+let percent = 0;
+let direction = 1;
+let newXY;
+//let yg1 = new yellowGuy(10, 10);
 
 function update() {
     ctx.fillRect(0, 0, can.width, can.height);
     
-	let yg1 = new yellowGuy(10, 10);
-	yg1.draw();
+	
+	//yg1.draw();
     
     let tr1 = new polygonalTrack(xy, xy, can.width / 2, can.height / 2);
 	tr1.setOriginXY(xy + 10 * p, xy + 10 * p);
@@ -70,5 +74,118 @@ function update() {
     if(p<20){
 
         p++;
+        
     }
 }
+animate();
+
+function animate(){
+
+    percent += direction;
+    if (percent >= 100){
+        percent = 0;
+    }
+
+     setTimeout(function () {
+         requestAnimationFrame(animate);
+     }, 1000 / FPS);
+    
+    blueGuy(percent);
+}
+function blueGuy(sliderValue) {
+
+    //side 1
+    ctx.clearRect(0, 0, can.width, can.height);
+
+    ctx.beginPath();
+    ctx.moveTo(230,230);
+    ctx.lineTo(230,690);
+    ctx.strokeStyle = 'red';
+    ctx.stroke();
+    //side 2
+    //ctx.clearRect(0, 0, can.width, can.height);
+    ctx.beginPath();
+    ctx.moveTo(230,690);
+    ctx.lineTo(690,690);
+    ctx.strokeStyle = 'red';
+    ctx.stroke();
+    // side 3
+    //ctx.clearRect(0, 0, can.width, can.height);
+    ctx.beginPath();
+    ctx.moveTo(690,690);
+    ctx.lineTo(690,230);
+    ctx.strokeStyle = 'red';
+    ctx.stroke();
+    // side 4
+    //ctx.clearRect(0, 0, can.width, can.height);
+    ctx.beginPath();
+    ctx.moveTo(690,230);
+    ctx.lineTo(230,230);
+    ctx.strokeStyle = 'red';
+    ctx.stroke();
+
+    //let xy;
+
+    if (sliderValue < 25) {
+        let percent = sliderValue / 24;
+        newXY = getLineXYatPercent({
+            x: 230,
+            y: 230
+        }, {
+            x: 230,
+            y: 690
+        }, percent);
+    }
+    else if (sliderValue < 50) {
+        let percent = sliderValue / 24;
+        newXY = getLineXYatPercent({
+            x: 230,
+            y: 690
+        }, {
+            x: 690,
+            y: 690
+        }, percent);
+    }
+    else if (sliderValue < 75) {
+        let percent = sliderValue / 24;
+        newXY = getLineXYatPercent({
+            x: 690,
+            y: 690
+        }, {
+            x: 690,
+            y: 230
+        }, percent);
+    }
+    else  {
+        let percent = sliderValue / 24;
+        newXY = getLineXYatPercent({
+            x: 690,
+            y: 230
+        }, {
+            x: 230,
+            y: 230
+        }, percent);
+    }
+    drawRect(newXY);
+}
+    function drawRect(point) {
+        ctx.fillStyle = "black";
+        ctx.strokeStyle = "gray";
+        ctx.lineWidth = 3;
+        ctx.beginPath();
+        ctx.rect(point.x - 13, point.y - 8, 25, 15);
+        ctx.fill();
+        ctx.stroke();
+    }
+
+    function getLineXYatPercent(startPt, endPt, percent) {
+        var dx = endPt.x - startPt.x;
+        var dy = endPt.y - startPt.y;
+        var X = startPt.x + dx * percent;
+        var Y = startPt.y + dy * percent;
+        return ({
+            x: X,
+            y: Y
+        });
+    }
+
