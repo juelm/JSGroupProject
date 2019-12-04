3327a89b12de0210d450c4b12127cebdd2f10f01
xobre <mua.breana@gmail.com>
2019-12-04 13:05:52 -0800

    update death

--- YellowGuy.js
+++ YellowGuy.js
@@ -12,45 +12,63 @@ export default class yellowGuy{
         this.color = '#ffff00';
         this.rotation = 2 * Math.PI;
         this.corners = [];
+        this.alive = true;
+        this.timeOfDeath = 0;
 	}
 
 	draw() {
+        if (this.side > 0){
+            if (this.alive === false){
+                this.timeOfDeath += 1;
+                if (this.timeOfDeath <= 30){
+                    this.side += 1;
+                }
+                if (this.timeOfDeath > 30){
+                    this.side -= 1;
+                }
+            }
 
-        if(this.rotation >= 2 * Math.PI) this.rotation = 0;
+            if(this.rotation >= 2 * Math.PI) this.rotation = 0;
 
-        let currentX = this.x + this.side / 2 * Math.cos(this.rotation);
-        let currentY = this.y + this.side / 2 * Math.sin(this.rotation);
+            let currentX = this.x + this.side / 2 * Math.cos(this.rotation);
+            let currentY = this.y + this.side / 2 * Math.sin(this.rotation);
 
-		ctx.strokeStyle = this.color;
-        
-        ctx.beginPath();
-        ctx.moveTo(currentX, currentY);
-        
-        ctx.lineTo(currentX + (this.side / 2) * Math.cos(this.rotation + .5 * Math.PI),
-                   currentY + (this.side / 2) * Math.sin(this.rotation + .5 * Math.PI));
+            ctx.strokeStyle = this.color;
+            
+            ctx.beginPath();
+            ctx.moveTo(currentX, currentY);
+            
+            ctx.lineTo(currentX + (this.side / 2) * Math.cos(this.rotation + .5 * Math.PI),
+                    currentY + (this.side / 2) * Math.sin(this.rotation + .5 * Math.PI));
 
-        currentX += (this.side / 2) * Math.cos(this.rotation + .5 * Math.PI);
-        currentY += (this.side / 2) * Math.sin(this.rotation + .5 * Math.PI);
+            currentX += (this.side / 2) * Math.cos(this.rotation + .5 * Math.PI);
+            currentY += (this.side / 2) * Math.sin(this.rotation + .5 * Math.PI);
 
 
-        ctx.lineTo(currentX + this.side * Math.cos(this.rotation + Math.PI),
-        currentY + this.side * Math.sin(this.rotation +  Math.PI));
+            ctx.lineTo(currentX + this.side * Math.cos(this.rotation + Math.PI),
+            currentY + this.side * Math.sin(this.rotation +  Math.PI));
 
-        currentX += this.side * Math.cos(this.rotation + Math.PI);
-        currentY += this.side * Math.sin(this.rotation + Math.PI);
+            currentX += this.side * Math.cos(this.rotation + Math.PI);
+            currentY += this.side * Math.sin(this.rotation + Math.PI);
 
-        ctx.lineTo(currentX + this.side * Math.cos(this.rotation + 1.5 * Math.PI),
-        currentY + this.side * Math.sin(this.rotation +  1.5 * Math.PI));
+            ctx.lineTo(currentX + this.side * Math.cos(this.rotation + 1.5 * Math.PI),
+            currentY + this.side * Math.sin(this.rotation +  1.5 * Math.PI));
 
-        currentX += this.side * Math.cos(this.rotation + 1.5 * Math.PI);
-        currentY += this.side * Math.sin(this.rotation + 1.5 * Math.PI);
+            currentX += this.side * Math.cos(this.rotation + 1.5 * Math.PI);
+            currentY += this.side * Math.sin(this.rotation + 1.5 * Math.PI);
 
-        ctx.lineTo(currentX + this.side * Math.cos(this.rotation + 2 * Math.PI),
-        currentY + this.side * Math.sin(this.rotation +  2 * Math.PI));
+            ctx.lineTo(currentX + this.side * Math.cos(this.rotation + 2 * Math.PI),
+            currentY + this.side * Math.sin(this.rotation +  2 * Math.PI));
 
-        ctx.closePath();
-        ctx.stroke();
+            ctx.closePath();
+            ctx.stroke();
 
-        this.rotation += turnSpeed; 
-	}
+            this.rotation += turnSpeed; 
+        }
+
+    }
+    death(){
+        this.alive = false;
+
+    }
 }
--- timer.js
+++
@@ -1,6 +0,0 @@
-   function countdown() {
-    let seconds = 15, $seconds = document.querySelector('gameCanvas');
-    $seconds.textContent = seconds
-    ctx.fillStyle = "red";
-    if(seconds --> 0) setTimeout(countdown, 1000)
-}
