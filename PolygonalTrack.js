c3f945b069e953ec128a0a063cecd33748e6334b
greymiba <greymiba@gmail.com>
2019-12-05 15:25:13 -0800

    Obstacles with hit detection. BlueGuy stops moving near obstacle upon hit.

--- PolygonalTrack.js
+++ PolygonalTrack.js
@@ -2,12 +2,46 @@ let can = document.getElementById('gameCanvas');
 let ctx = can.getContext('2d');
 
 export default class polygonalTrack {
-	constructor(x, y, width, height) {
+	constructor(x, y, width, height, obstaclesLvl=0) {
 		this.x = x;
 		this.y = y;
 		this.width = width;
 		this.height = height;
 		this.trackColor = 'cyan';
+		this.obstacleLvl=obstaclesLvl;
+		this.obstacleArray1=[];
+		this.populateObsArr();
+	}
+	populateObsArr(){
+		
+		let obs1=[this.x+120, this.y + this.height/3, this.x+220,this.y + this.height/3];
+		let obs2 = obs1.slice();
+		obs2[0]+=250;
+		obs2[1]+=this.height/3;
+		obs2[2]+=250;
+		obs2[3]+=this.height/3;
+		this.obstacleArray1[0]=obs1;
+		this.obstacleArray1[1]=obs2;
+
+	}
+	drawObstacles(){
+		let xStep = this.width / 4;
+		let yStep = this.height / 3;
+		ctx.strokeStyle = "red";
+		for(let a of this.obstacleArray1){
+
+			ctx.beginPath();
+			
+			ctx.moveTo(a[0], a[1]);
+			ctx.lineTo(a[2], a[3]);
+			ctx.closePath();
+			ctx.stroke();
+		}
+
+	}
+	getObstacleCoords(){
+		return(this.obstacleArray1);
+
 	}
 	drawF() {
 		ctx.strokeStyle = 'white';
@@ -23,6 +57,11 @@ export default class polygonalTrack {
 		ctx.lineTo(this.x, this.y + this.height);
 		ctx.closePath();
 		ctx.stroke();
+		if(this.obstacleLvl!==0){
+			this.populateObsArr();
+			this.drawObstacles();
+
+		}
 	}
 	getOriginXY() {
 		return [ this.x, this.y ];
--- index.js
+++ index.js
@@ -13,42 +13,58 @@ let ctx = can.getContext('2d');
 
 let timeCan = document.getElementById('timerCanvas');
 let timeCtx = timeCan.getContext('2d');
-timeCtx.fillRect(0,0,timeCan.width,timeCan.height);
+timeCtx.fillRect(0, 0, timeCan.width, timeCan.height);
 
 let xy = 40;
 let p = 0;
 let bgCoordinates;
-
-
+let obstacleCoord;
+let hitObstacle=false;
 
 setInterval(update, 1000 / FPS);
-let tr1 = new polygonalTrack(xy, xy, can.width / 2, can.height / 2);
-let yg1 = new yellowGuy(300, 300);
-let bg = new blueGuy(tr1.x, tr1.y);
+let tr1 = new polygonalTrack(xy, xy, can.width / 2, can.height / 2,1);
+//let yg1 = new yellowGuy(300, 300);
+let bg = new blueGuy();
 
 document.addEventListener('keydown', keydown);
 
-function keydown(ev) {
+function keydown(/** @type {keyboardEvent}*/ ev) {
 	switch (ev.keyCode) {
-		case 13: //Enter Key
-            bgCoordinates = bg.jump(); //Need coordinates of blueGuy to compare withcoordinates of yellowGuy.
-            checkHitStatus();
+		case 13:; //Enter Key
+		case 32:
+			bgCoordinates = bg.jump(); //Need coordinates of blueGuy to compare withcoordinates of yellowGuy.
+			obstacleCoord = tr1.getObstacleCoords();
+      checkobstacleStatus();
+			checkHitStatus();
 			break;
 	}
 }
 
-function checkHitStatus (){
-    let leeway=15;
-    for(let m of L1State){
-        if( (bgCoordinates[0]>m.x-leeway)  && (bgCoordinates[0] <m.x+leeway) || (bgCoordinates[1]>m.y-leeway)  && (bgCoordinates[1] <m.y+leeway)){
-            m.die();
-        }
-    }
+function checkHitStatus() {
+	let leeway = 15;
+	for (let m of L1State) {
+		if (
+			(bgCoordinates[0] > m.x - leeway && bgCoordinates[0] < m.x + leeway) ||
+			(bgCoordinates[1] > m.y - leeway && bgCoordinates[1] < m.y + leeway)
+		) {
+			m.die();
+		}
+	}
+}
+function checkobstacleStatus(){
+
+	for(let a of obstacleCoord){
+			if( (bgCoordinates[0]>a[0])  && (bgCoordinates[0] <a[2]) || (bgCoordinates[1]>a[1])  && (bgCoordinates[1] <a[3])){
+
+					bg.die();
+					hitObstacle=true;
+			}
+
+	}
 }
 
-
 function createRow(track, y, arr) {
-	for (let i = track.x + 50; i < track.x + track.width; i += 50) {
+	for (let i = track.x + 75; i < track.x + track.width; i += 50) {
 		let temp = new yellowGuy(i, y);
 		arr.push(temp);
 	}
@@ -56,81 +72,61 @@ function createRow(track, y, arr) {
 
 function update() {
 	ctx.fillRect(0, 0, can.width, can.height);
-    tr1.draw();
+	tr1.setOriginXY(xy + 10 * p, xy + 10 * p);
+	tr1.draw();
+	if (p < 20) {
+		p++;
+	}
 
-    if(p<20){
-        p++;
-        tr1.setOriginXY(xy + 10 * p, xy + 10 * p);
-    }
+	if (p === 20) {
+		bg.x = tr1.x;
+		bg.y = tr1.y;
+		p++;
+		createRow(tr1, tr1.y + tr1.height / 2, L1State);
+		createRow(tr1, tr1.y + tr1.height / 2 + 50, L1State);
+	}
+	if (p > 20) {
+		let allDone = true;
 
-    if(p === 20) {
-        bg.x = tr1.x;
-        bg.y = tr1.y
-        p++;
-        createRow(tr1,tr1.y+tr1.height/2, L1State);
-        createRow(tr1,tr1.y+tr1.height/2 + 50, L1State);
+		for (let i = 0; i < L1State.length; i++) {
+			L1State[i].draw();
+			if (L1State[i].alive) allDone = false;
+		}
 
-        
-    }
-    if(p === 21){
+		if (allDone) bg.win();
 
-        let allDone = true;
+		bg.draw();
+		bg.move(tr1);
 
-        for(let i = 0; i < L1State.length; i++) {
-            L1State[i].draw();
-            if(L1State[i].alive) allDone = false; 
-        }
+		timeCtx.clearRect(0, 0, timeCan.width, timeCan.height);
+		timeCtx.fillRect(0, 0, timeCan.width, timeCan.height);
+		countdown();
+		secondTimer++;
+		if (secondTimer == FPS) {
+			if (!allDone) gameTimer--;
 
-        if(allDone) bg.win();
+			secondTimer = 0;
+		}
+		if (gameTimer <= 0) {
+			gameTimer = 0;
+			bg.die();
 
-        bg.draw();
-        bg.move(tr1);
-
-        timeCtx.clearRect(0,0,timeCan.width,timeCan.height);
-        timeCtx.fillRect(0,0,timeCan.width,timeCan.height);
-        countdown();
-        secondTimer ++;
-        if (secondTimer == FPS){
-
-            if(!allDone) gameTimer--;
-
-            secondTimer = 0;
-        }
-        if (gameTimer <= 0){
-            gameTimer = 0;
-            bg.die();
-
-            for (let j = 0; j < L1State.length; j++){
-                L1State[j].die();
-            }
-            setTimeout(() => {gameTimer = 10; 
-                              secondTimer = 0; 
-                              L1State = []; 
-                              p = 0; 
-                              bg = new blueGuy(tr1.x, tr1.y); 
-                              tr1 = new polygonalTrack(xy, xy, can.width / 2, can.height / 2)
-                            }, 3000);
-
-            p++;
-            
-        }
-    }
-    if(p === 22){
-        bg.draw();
-        for(let i = 0; i < L1State.length; i++) {
-            L1State[i].draw();
-        }
-
-    }
+			for (let j = 0; j < L1State.length; j++) {
+				L1State[j].die();
+			}
+			setTimeout(() => {
+				gameTimer = 10;
+				p = 0;
+				hitObstacle=false;
+				bg = new blueGuy();
+			}, 3000);
+		}
+	}
 }
 
 function countdown() {
-
-    timeCtx.font = "50px Verdana";
-    timeCtx.fillStyle = "magenta";
-    timeCtx.fillText(gameTimer, 100, 90);
-    timeCtx.fillStyle = "black";
-
+	timeCtx.font = '50px Verdana';
+	timeCtx.fillStyle = 'magenta';
+	timeCtx.fillText(gameTimer, 100, 90);
+	timeCtx.fillStyle = 'black';
 }
-
-  
